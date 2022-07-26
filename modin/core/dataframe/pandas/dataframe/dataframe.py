c5b6ce6a356b7e6e0ae23fe34bfd11425a631b52
Anatoly Myachev <anatoly.myachev@intel.com>
2022-07-26 14:47:22 -0500

    left-join perf fix
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- modin/core/dataframe/pandas/dataframe/dataframe.py
+++ modin/core/dataframe/pandas/dataframe/dataframe.py
@@ -1844,6 +1844,7 @@ class PandasDataframe(ClassLogger):
         new_index=None,
         new_columns=None,
         dtypes=None,
+        check_axes_sync=True,
     ):
         """
         Perform a function across an entire axis.
@@ -1864,6 +1865,12 @@ class PandasDataframe(ClassLogger):
             The data types of the result. This is an optimization
             because there are functions that always result in a particular data
             type, and allows us to avoid (re)computing it.
+        check_axes_sync : bool, default: True
+            In some cases, synchronization of the external index with the internal
+            indexes of partitions is not required, since when performing certain
+            functions, we know in advance that it must match. A good example is the
+            `reindex` function. Although synchronization is performed asynchronously,
+            it is nevertheless a rather expensive operation.
 
         Returns
         -------
@@ -1881,6 +1888,7 @@ class PandasDataframe(ClassLogger):
             new_columns=new_columns,
             dtypes=dtypes,
             other=None,
+            check_axes_sync=check_axes_sync,
         )
 
     @lazy_metadata_decorator(apply_axis="both")
@@ -2254,6 +2262,7 @@ class PandasDataframe(ClassLogger):
         apply_indices=None,
         enumerate_partitions=False,
         dtypes=None,
+        check_axes_sync=True,
     ):
         """
         Broadcast partitions of `other` Modin DataFrame and apply a function along full axis.
@@ -2281,6 +2290,12 @@ class PandasDataframe(ClassLogger):
             Data types of the result. This is an optimization
             because there are functions that always result in a particular data
             type, and allows us to avoid (re)computing it.
+        check_axes_sync : bool, default: True
+            In some cases, synchronization of the external index with the internal
+            indexes of partitions is not required, since when performing certain
+            functions, we know in advance that it must match. A good example is the
+            `reindex` function. Although synchronization is performed asynchronously,
+            it is nevertheless a rather expensive operation.
 
         Returns
         -------
@@ -2327,9 +2342,10 @@ class PandasDataframe(ClassLogger):
             None,
             dtypes,
         )
-        if new_index is not None:
+        # There is can be case where synchronization is not needed
+        if check_axes_sync and new_index is not None:
             result.synchronize_labels(axis=0)
-        if new_columns is not None:
+        if check_axes_sync and new_columns is not None:
             result.synchronize_labels(axis=1)
         return result
 
--- modin/pandas/test/dataframe/test_join_sort.py
+++ modin/pandas/test/dataframe/test_join_sort.py
@@ -158,6 +158,100 @@ def test_join(test_data, test_data2):
         df_equals(modin_join, pandas_join)
 
 
+def test_join_4427():
+    count_cols = 16
+    data = np.random.uniform(0, 100, size=(1_000, count_cols))
+    left_columns = [f"lcol{x}" for x in range(count_cols)]
+    right_columns = [f"rcol{x}" for x in range(count_cols)]
+    modin_df1 = pd.DataFrame(data, columns=left_columns)
+    modin_df2 = pd.DataFrame(data, columns=right_columns)
+
+    pandas_df1 = pandas.DataFrame(data, columns=left_columns)
+    pandas_df2 = pandas.DataFrame(data, columns=right_columns)
+
+    for on in (None, left_columns[0], left_columns[:2]):
+        # check general `join` code branch
+        eval_general(
+            modin_df1,
+            pandas_df1,
+            lambda df: df.join(
+                modin_df2.set_index(right_columns[:2])
+                if isinstance(df, pd.DataFrame)
+                else pandas_df2.set_index(right_columns[:2]),
+                on=on,
+            ),
+        )
+
+        # check exception: 'len(left_on) must equal the number of levels in the index of "right"'
+        eval_general(
+            modin_df1,
+            pandas_df1,
+            lambda df: df.join(
+                modin_df2 if isinstance(df, pd.DataFrame) else pandas_df2,
+                on=on,
+            ),
+        )
+
+    # check exception when labels in index and columns intersect
+    new_index = pandas.MultiIndex.from_frame(pandas_df1[left_columns[:2]])
+    modin_df1.index = new_index
+    pandas_df1.index = new_index
+
+    eval_general(
+        modin_df1,
+        pandas_df1,
+        lambda df: df.join(
+            modin_df2.set_index(right_columns[:3])
+            if isinstance(df, pd.DataFrame)
+            else pandas_df2.set_index(right_columns[:3]),
+            on=left_columns[:3],
+        ),
+    )
+
+    # check the case where all labels in `on` are just all names in MultiIndex (fast path)
+    new_index = pandas.MultiIndex.from_frame(
+        pandas_df1[left_columns[:2]], names=["name1", "name2"]
+    )
+    modin_df1.index = new_index
+    pandas_df1.index = new_index
+
+    eval_general(
+        modin_df1,
+        pandas_df1,
+        lambda df: df.join(
+            modin_df2.set_index(right_columns[:2])
+            if isinstance(df, pd.DataFrame)
+            else pandas_df2.set_index(right_columns[:2]),
+            on=new_index.names,
+        ),
+    )
+
+    # check the case where all labels in `on` are just part of names in MultiIndex
+    # and part of columns names
+    eval_general(
+        modin_df1,
+        pandas_df1,
+        lambda df: df.join(
+            modin_df2.set_index(right_columns[:2])
+            if isinstance(df, pd.DataFrame)
+            else pandas_df2.set_index(right_columns[:2]),
+            on=new_index.names[:1] + left_columns[:1],
+        ),
+    )
+
+    # check the case where all labels in `on` are just part of names in MultiIndex
+    eval_general(
+        modin_df1,
+        pandas_df1,
+        lambda df: df.join(
+            modin_df2.set_index(right_columns[:1])
+            if isinstance(df, pd.DataFrame)
+            else pandas_df2.set_index(right_columns[:1]),
+            on=new_index.names[:1],
+        ),
+    )
+
+
 @pytest.mark.parametrize(
     "test_data, test_data2",
     [
