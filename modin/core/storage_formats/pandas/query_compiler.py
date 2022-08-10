22493a835685fd2f16d7ae37bcfebefd8dd35293
proxodilka <zeeron209@gmail.com>
2022-08-10 15:45:12 +0300

    Add a switch between FullAxis and MapReduce groupby
    Signed-off-by: proxodilka <zeeron209@gmail.com>

--- modin/core/storage_formats/pandas/query_compiler.py
+++ modin/core/storage_formats/pandas/query_compiler.py
@@ -2785,7 +2785,8 @@ class PandasQueryCompiler(BaseQueryCompiler):
         how="axis_wise",
         drop=False,
     ):
-        if isinstance(agg_func, dict) and all(
+        force_full_axis = agg_kwargs.pop("force_full_axis", False)
+        if not force_full_axis and isinstance(agg_func, dict) and all(
             is_reduce_function(x) for x in agg_func.values()
         ):
             return self._groupby_dict_reduce(
